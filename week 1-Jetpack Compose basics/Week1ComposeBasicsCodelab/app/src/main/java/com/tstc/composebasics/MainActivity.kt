8908a6be7b14e566c05108841456f50678fcd98d
TeamSTC <teamsirothecat@gmail.com>
2021-11-05 10:54:10 +0900

    Week1, 12. Styling and theming your app

--- week 1-Jetpack Compose basics/Week1ComposeBasicsCodelab/app/src/main/java/com/tstc/composebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1ComposeBasicsCodelab/app/src/main/java/com/tstc/composebasics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tstc.composebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tstc.composebasics.ui.theme.ComposeBasicsTheme
@@ -87,7 +89,13 @@ fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ){
                 Text(text = "Hello,")
-                Text(text = "$name!")
+                //Text(text = "$name!")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 onClick = { expanded = !expanded }
@@ -102,11 +110,17 @@ fun Greeting(name: String) {
 
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeBasicsTheme {
-        MyApp()
+        Greetings()
     }
 }
 
