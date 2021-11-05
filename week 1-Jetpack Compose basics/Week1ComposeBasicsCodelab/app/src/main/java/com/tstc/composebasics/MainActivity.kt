87d0a7265229a5518553e38c2780e13c1ef57b0e
TeamSTC <teamsirothecat@gmail.com>
2021-11-05 10:21:26 +0900

    Week1, 10. Persisting state

--- week 1-Jetpack Compose basics/Week1ComposeBasicsCodelab/app/src/main/java/com/tstc/composebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1ComposeBasicsCodelab/app/src/main/java/com/tstc/composebasics/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -44,7 +45,7 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun MyApp() {
 
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
